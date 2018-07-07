23f654ee14a7557b487b576d6a9db3dd15c5a332
Alexey Ryabov <6006l1k@gmail.com>
2018-07-07 11:10:50 +0300

    Initial commit

---
+++ schema.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS stat;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE stat (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    created TEXT NOT NULL,
+    total INTEGER NOT NULL,
+    added INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
